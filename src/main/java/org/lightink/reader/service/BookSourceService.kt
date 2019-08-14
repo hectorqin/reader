8cc5be2800a2da654b857b92d0ccace59e99591c
zoharSoul <hunji66@gmail.com>
2019-08-14 19:23:02 +0800

    :sparkles: cache expire

--- src/main/java/org/lightink/reader/service/BookSourceService.kt
+++ src/main/java/org/lightink/reader/service/BookSourceService.kt
@@ -36,7 +36,7 @@ class BookSourceService {
     @Value("\${qingmo.server.url}")
     private lateinit var serviceUrl: String
 
-    private val cache = CacheBuilder.newBuilder().refreshAfterWrite(30, TimeUnit.MINUTES).build<String, Any>()
+    private val cache = CacheBuilder.newBuilder().expireAfterWrite(30, TimeUnit.MINUTES).build<String, Any>()
 
     fun bookSourceRepositoryList(): Single<List<BookSourceRepository>> {
 
